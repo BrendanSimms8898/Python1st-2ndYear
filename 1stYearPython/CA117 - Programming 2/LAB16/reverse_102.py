cb6ffcb317717e31b476659616b8fde666c4d8e1
simmsb3 <brendan.simms3@mail.dcu.ie>
2021-01-29 22:28:57 +0000

    Cleaned tthe Repo  and Reoragnised

--- 1stYearPython/CA117 - Programming 2/LAB16/reverse_102.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-def reverse_list(l = []):
-	if len(l) == 0:
-		return []
-	else:
-		return [l[-1]] + reverse_list(l[:-1])
--- CA268/SortingDetective.py
+++
@@ -1,2 +0,0 @@
-def sort_algs():
-    return ['is', 'qs', 'ms', 'ss']
