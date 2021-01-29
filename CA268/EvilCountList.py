93009f30668937965ac9c9b5906fc26319ccd1b1
Brendan Simms <77291876+BrendanSimms8898@users.noreply.github.com>
2021-01-29 19:18:48 +0000

    Update EvilCountList.py
    Added commentation and test code

--- CA268/EvilCountList.py
+++ CA268/EvilCountList.py
@@ -1,3 +1,5 @@
+#function which takes a linked list of integers as an argument and counts the number of even elements in the list
+
 def even_count(lst):
 	nums = 0
 	while not lst.is_empty():
@@ -5,3 +7,21 @@ def even_count(lst):
 		if n % 2 == 0:
 			nums += 1
 	return nums
+
+#Code to test provided by lecturer
+
+def main():
+    # Read each set
+    line = sys.stdin.readline()
+    items = line.strip().split()
+    nums = [int(item) for item in items]
+    
+    ll = LinkedList()
+    
+    for num in nums:
+        ll.add(num)
+    
+    print(even_count(ll))
+
+if __name__ == "__main__":
+    main()
