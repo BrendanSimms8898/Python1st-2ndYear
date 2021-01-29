7eed5f00b840a2fdde29387ca31c43ce39315fd3
Brendan Simms <77291876+BrendanSimms8898@users.noreply.github.com>
2021-01-29 19:16:15 +0000

    Update WhosBefore.py
    Added commnetation and Main() to test

--- CA268/WhosBefore.py
+++ CA268/WhosBefore.py
@@ -47,7 +47,8 @@ class LinkedList:
                 return ptr.next.item
             ptr=ptr.next
         return None
-
+#Add a before() method to the LinkedList class which takes as parameter an item and returns the item that occurs before it in the linked list. 
+#If no such item exists, then return None.
     def before(self,e):
         ptr=self.head
         if ptr!=None:
@@ -57,3 +58,32 @@ class LinkedList:
                 ptr=ptr.next
         
         return None
+    
+  #main function to test
+def main():
+    # Read each set
+    line = sys.stdin.readline()
+    items = line.strip().split()
+    
+    # A list to store the results of the tests
+    tests = []
+    
+    ll = LinkedList()
+
+    # Check that it works for an empty list    
+    tests.append(ll.before("") == None)  # Each test should be True
+
+    # Check that the item doesn't exist before it is added    
+    for item in items:
+        tests.append(ll.before(item) == None)
+        ll.add(item)
+    
+    items.reverse()
+    for i in range(len(items) - 1):
+        # print(ll.before(items[i + 1]), items[i])
+        tests.append(ll.before(items[i + 1]) == items[i])
+        
+    print("All Good" if all(tests) else str(tests))
+
+if __name__ == "__main__":
+    main()
