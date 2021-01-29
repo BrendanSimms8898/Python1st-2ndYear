86467b5360101393a4f6bbee549d2a729c1ec73f
Brendan Simms <77291876+BrendanSimms8898@users.noreply.github.com>
2021-01-29 18:59:17 +0000

    Update ReversingtheInput.py
    Added commentation and test codes

--- CA268/ReversingtheInput.py
+++ CA268/ReversingtheInput.py
@@ -6,3 +6,37 @@ def reverse_input(stack):
         stack.push(line.strip())
     while not stack.is_empty():
         print(stack.pop())
+
+#Example stack class:
+class Stack:
+#
+#  Stack ADT has three methods: is_empty, push and pop.
+#
+   def __init__(self):
+      self.stack = []
+      self.top = 0
+
+   def is_empty(self):
+      return self.top == 0
+
+   def push(self, item):
+      if self.top < len(self.stack):
+         self.stack[self.top] = item
+      else:
+         self.stack.append(item)
+
+      self.top += 1
+
+   def pop(self):
+      if self.is_empty():
+         return None
+      else:
+         self.top -= 1
+         return self.stack[self.top]
+        
+ #example code to test function
+from Stack import Stack
+from reverse_input import reverse_input
+
+stack = Stack()
+reverse_input(stack)
