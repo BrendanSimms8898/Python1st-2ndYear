16b668b0cef8c914cc6193d1231d99fef08de665
Brendan Simms <77291876+BrendanSimms8898@users.noreply.github.com>
2021-01-29 18:48:41 +0000

    Update UniqueElementsofList.py
    Added commentation and Test code

--- CA268/UniqueElementsofList.py
+++ CA268/UniqueElementsofList.py
@@ -1,2 +1,21 @@
 def unique_list(a):
     return " ".join(set(a)).split()
+
+#Test code provided by lecturer:
+import sys
+from sets import unique_list
+
+def main():
+    # Read in a list of strings
+    lst = sys.stdin.readline().strip().split()
+
+    # call the student's function ...
+    answer = unique_list(lst)
+    print(type(answer)) # should be a list
+    answer.sort()
+    print(answer)
+
+if __name__ == "__main__":
+    main()
+    
+#unique_list function returns a new list containing only the unique elements of the passed list
